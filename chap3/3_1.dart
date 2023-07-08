00ce19256783a7d9e47c4de76499e5e709bc1e17
drkooo <admin@drkooo.com>
2023-07-08 08:59:55 +0800

    chap3/3_1.dart

--- chap3/3_1.dart
+++ chap3/3_1.dart
@@ -1,8 +1,27 @@
 void main() {
-  untypedVariables();
+  stringPlayground();
 }
 
-void untypedVariables() {
-  dynamic something = 14.2;
-  print(something.runtimeType);
+void stringPlayground() {
+  basicStringDeclaration();
+  //multiLineStrings();
+  //combiningStrings();
+}
+
+void basicStringDeclaration() {
+  print('Single quotes');
+  final aBoldStatement = 'Dart isn\'t loosely typed.';
+  print(aBoldStatement);
+//raw string
+  print('Raw String');
+  final rawString = r'Show an escape \ character';
+  print(rawString);
+  // With Double Quotes
+  print("Hello, World");
+  final aMoreMildOpinion = "Dart's popularity has skyrocketed with Flutter!";
+  print(aMoreMildOpinion);
+// Combining single and double quotes
+  final mixAndMatch =
+      'Every programmer should write "Hello, World" when learning a new language.';
+  print(mixAndMatch);
 }
