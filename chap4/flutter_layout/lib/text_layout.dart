445c15885ae5cede1071daa4dba0ecb712bc7a29
drkooo <admin@drkooo.com>
2023-11-21 23:56:35 +0800

    text_layout.dart

---
+++ chap4/flutter_layout/lib/text_layout.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class TextLayout extends StatelessWidget {
+  const TextLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 36),
+        ),
+        Text(
+          'Text can wrap without issue.',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const Text('Lorem ipsum dolor sit amet,'
+            ' consectetur adipiscing elit. Etiam at mauris massa. '
+            'Suspendisse potenti. Aenean aliquet '
+            'eunisl vitae tempus.'),
+        const Divider(),
+        RichText(
+          text: const TextSpan(
+            text: 'Flutter text is ',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.black,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'really ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'powerful.',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.double,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
